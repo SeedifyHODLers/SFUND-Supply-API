f3574cfff402416d5376ffcc6c49f74b0c6542a5
Maxime Guermonprez <m.guermonprez@certigna.com>
2021-12-29 17:18:53 +0100

    Adding 90 days staking

--- src/app.ts
+++ src/app.ts
@@ -42,7 +42,7 @@ const options = {
 };
 
 const web3 = new Web3(new Web3.providers.HttpProvider("https://bsc-dataseed1.binance.org:443", options));
-const seedifyLockedPoolAddresses: string[] = [process.env.LOCKED_STAKING_7D, process.env.LOCKED_STAKING_14D, process.env.LOCKED_STAKING_30D, process.env.LOCKED_STAKING_60D].filter(addr => addr !== undefined) as string[]
+const seedifyLockedPoolAddresses: string[] = [process.env.LOCKED_STAKING_7D, process.env.LOCKED_STAKING_14D, process.env.LOCKED_STAKING_30D, process.env.LOCKED_STAKING_60D, process.env.LOCKED_STAKING_90D].filter(addr => addr !== undefined) as string[]
 const apeStakingAddress: string | undefined = process.env.APE_STAKING
 const apeFarmingAddress: string | undefined = process.env.APE_FARM
 const seedifyLockedFarmPoolAddresses: string[] = [process.env.LOCKED_FARM_CAKE_LP, process.env.LOCKED_FARM_CAKE_LP_OLD, process.env.LOCKED_FARM_BAKE_LP, process.env.LOCKED_FARM_BAKE_LP_OLD].filter(addr => addr !== undefined) as string[]
